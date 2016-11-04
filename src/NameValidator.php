1fd43cb066f4b1e46c78aef710d7242af605f344
kiler129 <grzegorz@noflash.pl>
2016-11-04 01:20:31 -0500

    Added copyright notes

--- src/NameValidator.php
+++ src/NameValidator.php
@@ -1,5 +1,12 @@
 <?php
-
+/**
+ * This file is part of FunctionInjector project.
+ * You are using it at your own risk and you are fully responsible for everything that code will do.
+ *
+ * Copyright (c) 2016 Grzegorz Zdanowski <grzegorz@noflash.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE file distributed with this source code.
+ */
 
 namespace noFlash\FunctionsManipulator;
 
