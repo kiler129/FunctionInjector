10322c7516b62d4e3145ad1f6227767a6c5c8171
kiler129 <grzegorz@noflash.pl>
2017-08-16 00:10:03 -0500

    Added internal LogicException

---
+++ src/Exception/LogicException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This file is part of FunctionInjector project.
+ * You are using it at your own risk and you are fully responsible for everything that code will do.
+ *
+ * Copyright (c) 2017 Grzegorz Zdanowski <grzegorz@noflash.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE file distributed with this source code.
+ */
+
+namespace noFlash\FunctionsManipulator\Exception;
+
+class LogicException extends \LogicException
+{
+
+}
---
+++ tests/Exception/LogicExceptionTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This file is part of FunctionInjector project.
+ * You are using it at your own risk and you are fully responsible for everything that code will do.
+ *
+ * Copyright (c) 2017 Grzegorz Zdanowski <grzegorz@noflash.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE file distributed with this source code.
+ */
+
+namespace noFlash\FunctionsManipulator\Tests\Exception;
+
+use noFlash\FunctionsManipulator\Exception\LogicException;
+
+class LogicExceptionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @testdox Class extends \LogicException
+     */
+    public function testClassExtendsLogicException()
+    {
+        $this->assertTrue(
+            is_subclass_of(LogicException::class, \LogicException::class)
+        );
+    }
+}
