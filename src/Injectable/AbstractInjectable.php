e2cda8a27fb71bd16a506b0f7c38a34065affe43
kiler129 <grzegorz@noflash.pl>
2017-08-15 21:06:57 -0500

    Extracted reusable functionality for InjectableInterface

---
+++ src/Injectable/AbstractInjectable.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * This file is part of FunctionInjector project.
+ * You are using it at your own risk and you are fully responsible for everything that code will do.
+ *
+ * Copyright (c) 2017 Grzegorz Zdanowski <grzegorz@noflash.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE file distributed with this source code.
+ */
+
+namespace noFlash\FunctionsManipulator\Injectable;
+
+use noFlash\FunctionsManipulator\Exception\InvalidFunctionNameException;
+use noFlash\FunctionsManipulator\Exception\InvalidNamespaceException;
+use noFlash\FunctionsManipulator\NameValidator;
+
+abstract class AbstractInjectable implements InjectableInterface
+{
+    /**
+     * @var NameValidator
+     */
+    private $nameValidator;
+
+    /**
+     * @var string
+     */
+    protected $namespace;
+
+    /**
+     * @var string
+     */
+    protected $functionName;
+
+    public function __construct()
+    {
+        $this->nameValidator = new NameValidator();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getNamespace()
+    {
+        return $this->namespace;
+    }
+
+    public function setNamespace($ns)
+    {
+        if (!is_string($ns)) {
+            throw new \InvalidArgumentException(
+                "Namespace expected to be a string, got $ns"
+            );
+        }
+
+        $valid = $this->nameValidator->validateNamespace($ns);
+        if ($valid !== NameValidator::RESULT_OK) {
+            throw new InvalidNamespaceException($ns, $valid);
+        }
+
+        $this->namespace = $ns;
+
+        return $this;
+    }
+
+    public function getFunctionName()
+    {
+        return $this->functionName;
+    }
+
+    public function setFunctionName($name)
+    {
+        $valid = $this->nameValidator->validateFunctionName($name);
+        if ($valid !== NameValidator::RESULT_OK) {
+            throw new InvalidFunctionNameException($name, $valid);
+        }
+
+        $this->functionName = $name;
+
+        return $this;
+    }
+}
--- src/Injectable/InjectableCallback.php
+++ src/Injectable/InjectableCallback.php
@@ -15,80 +15,23 @@ use noFlash\FunctionsManipulator\Exception\InvalidFunctionNameException;
 use noFlash\FunctionsManipulator\Exception\InvalidNamespaceException;
 use noFlash\FunctionsManipulator\NameValidator;
 
-class InjectableCallback implements InjectableInterface
+class InjectableCallback extends AbstractInjectable
 {
-    /**
-     * @var NameValidator
-     */
-    private $nameValidator;
-
-    /**
-     * @var string
-     */
-    private $namespace;
-
-    /**
-     * @var string
-     */
-    private $functionName;
-
     /**
      * @var callable
      */
     private $callback;
 
-    public function __construct()
-    {
-        $this->nameValidator = new NameValidator();
-        $this->callback = function () {
-            throw new IncompleteInjectableException(
-                'Injectable callback was invoked, but no callback was set'
-            );
-        };
-    }
-
-    public function getNamespace()
-    {
-        return $this->namespace;
-    }
-
-    public function setNamespace($ns)
-    {
-        if (!is_string($ns)) {
-            throw new \InvalidArgumentException(
-                "Namespace expected to be a string, got $ns"
-            );
-        }
-
-        $valid = $this->nameValidator->validateNamespace($ns);
-        if ($valid !== NameValidator::RESULT_OK) {
-            throw new InvalidNamespaceException($ns, $valid);
-        }
-
-        $this->namespace = $ns;
-
-        return $this;
-    }
-
-    public function getFunctionName()
-    {
-        return $this->functionName;
-    }
-
-    public function setFunctionName($name)
-    {
-        $valid = $this->nameValidator->validateFunctionName($name);
-        if ($valid !== NameValidator::RESULT_OK) {
-            throw new InvalidFunctionNameException($name, $valid);
-        }
-
-        $this->functionName = $name;
-
-        return $this;
-    }
-
     public function getCallback()
     {
+        if ($this->callback === null) {
+            return function () {
+                throw new IncompleteInjectableException(
+                    'Injectable callback was invoked, but no callback was set'
+                );
+            };
+        }
+
         return $this->callback;
     }
 
